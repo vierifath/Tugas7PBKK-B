49434cf50fdcd782087b1bc414075849e7bee810
vierifath <vieri.ayuba@gmail.com>
2021-07-07 18:58:21 +0700

    add

---
+++ crud/application/views/template/footer.php
@@ -0,0 +1,9 @@
+	<footer class="footer">
+	  <div class="container" style="text-align:center;">
+	    <hr />
+	    <span>Vieri Fath Ayuba - PBKK B @<?php echo date('Y'); ?></span>
+	  </div>
+	</footer>
+	</body>
+
+	</html>
